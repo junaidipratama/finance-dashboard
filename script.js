788d76106a0d1cda2e6c37ff34a76ab3e4ce4998
junaidipratama <junaidipratama39@gmail.com>
2026-04-07 22:13:44 +0900

    Update chart data and add local storage functionality
    Updated chart labels and dataset values for financial data visualization. Added functionality to save financial data to local storage.

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const ctx = document.getElementById('pieChart');
 new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ['Saham', 'Crypto', 'Cash'],
+    labels: ['SAHAM', 'BITCOIN', 'EMAS', 'RDPU' ],
     datasets: [{
-      data: [5000000, 2000000, 1000000],
+      data: [20000000, 23000000, 6000000, 50000000],
     }]
   }
 });
+let dataKeuangan = JSON.parse(localStorage.getItem("dataKeuangan")) || [
+  { kategori: "SAHAM", jumlah: 20000000 },
+  { kategori: "BITCOIN", jumlah: 23000000 },
+  { kategori: "EMAS", jumlah: 6000000 },
+  { kategori: "RDPU", jumlah: 50000000 }
+];
+
+function simpanData() {
+  localStorage.setItem("dataKeuangan", JSON.stringify(dataKeuangan));
+}
